354004925ae4ba88e71210788d428527b0576c4b
themagiche <magiche.mc@gmail.com>
2020-12-02 16:13:46 +0300

    fixed admin

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
-// Import main css
-import '~/assets/style/index.scss'
 // Import global styles
 require('~/main.css')
+
+// Import main css
+import '~/assets/style/index.scss'
 
 import DefaultLayout from '~/layouts/Default.vue'
 import BlogLayout from '~/layouts/Blog.vue'
